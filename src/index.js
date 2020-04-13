40840748c8877a2268507e288ecbd6f4885e217e
goobong <bongbonggg97@gmail.com>
2020-04-13 23:36:23 +0900

    xIsNext 추가해서 x,o 번갈아 출력

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const Square = (props) => {
   return (
     <button
       className="square"
-      onClick={ () => props.onClick({value: 'X'}) }
+      onClick={props.onClick}
     >
       {props.value}
     </button>
@@ -17,14 +17,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X'
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,      
+    })
   }
 
   renderSquare(i) {
@@ -37,7 +41,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`;
 
     return (
       <div>
